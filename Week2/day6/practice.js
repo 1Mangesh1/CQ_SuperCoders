db0dbd0b779d0036a79bafa5cc439878a82a19e0
Mangesh Bide <mangeshbide123@gmail.com>
2023-07-09 18:08:27 +0530

    all

---
+++ Week2/day6/practice.js
@@ -0,0 +1,47 @@
+//callback function
+
+
+// console.log("start");
+
+// // setTimeout( function cod() {
+// //     console.log("Mangesh")
+    
+// // }, 3000);
+
+// console.log("end");
+
+// function add (n1,n2,callback){
+//     console.log(n1+n2);
+//     callback();
+// }
+
+// function gta (){
+//     console.log("bruh");
+// }
+// add(10,5,gta);
+
+// function fetchData(callback) {
+//     setTimeout(function() {
+//       const data = "Hello, world!";
+//       callback(data);
+//     }, 2000);
+//   }
+  
+//   function processResult(data) {
+//     console.log("Received data: " + data);
+//   }
+  
+//   fetchData(processResult);
+// function makeData(callback) {
+// setTimeout(function()   {
+//     console.log("data is ready")
+//     callback();
+// }, 4000);
+// }
+
+// function processResult() {
+//     console.log("data is processed");
+// }
+
+// makeData(processResult);
+
