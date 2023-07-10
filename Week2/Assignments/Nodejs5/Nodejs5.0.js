14e9ef68d8e720e6741fa860401f693cb6bf3af9
Mangesh Bide <mangeshbide123@gmail.com>
2023-07-10 16:26:52 +0530

    done done async await

--- Week2/Assignments/Nodejs5/Nodejs5.0.js
+++ Week2/Assignments/Nodejs5/Nodejs5.0.js
@@ -1,49 +1,49 @@
-const fs = require('fs');
-const https = require('https');
+const fs = require("fs");
+const https = require("https");
 
-// Function to make an HTTP request and return a Promise
 function makeHttpRequest(url) {
-  return new Promise((resolve, reject) => {
-    https.get(url, (response) => {
-      let data = '';
-      
-      response.on('data', (chunk) => {
-        data += chunk;
-      });
-  
-      response.on('end', () => {
-        resolve(data);
-      });
-  
-      response.on('error', (error) => {
+  return new Promise(function (resolve, reject) {
+    https
+      .get(url, function (response) {
+        let data = "";
+
+        response.on("data", function (chunk) {
+          data += chunk;
+        });
+
+        response.on("end", function () {
+          resolve(data);
+        });
+
+        response.on("error", function (error) {
+          reject(error);
+        });
+      })
+      .on("error", function (error) {
         reject(error);
       });
-    }).on('error', (error) => {
-      reject(error);
-    });
   });
 }
 
-// Function to read file, fetch URLs, and process responses
 async function fetchDataFromFile(filePath) {
   try {
-    const fileData = fs.readFileSync(filePath, 'utf8');
-    const urls = fileData.split('\n');
-
+    const fileData = fs.readFileSync(filePath, "utf8");
+    const urls = fileData.split("\n");
+    
     for (let i = 0; i < urls.length; i++) {
       const url = urls[i].trim();
-      if (url !== '') {
+      if (url !== "") {
         const response = await makeHttpRequest(url);
-        const fileName = `url_${i}.json`;
+        var x = i.toString();
+        const fileName = "url_" + x + ".json";
         fs.writeFileSync(fileName, response);
-        console.log(`Data from ${url} saved to ${fileName}`);
+        console.log("data from ", url, "saved to", fileName);
       }
     }
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error occured while receiving data : ", error);
   }
 }
 
-// Usage example
-const filePath = 'urls.txt';
+const filePath = "urls.txt";
 fetchDataFromFile(filePath);
--- Week2/Assignments/Nodejs5/Nodejs5.1.js
+++ Week2/Assignments/Nodejs5/Nodejs5.1.js
@@ -1,40 +1,40 @@
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 
-// Function to make an HTTP request and return a Promise
 function makeHttpRequest(url) {
-  const protocolModule = url.startsWith('https://') ? https : http;
+  const protocolModule = url.startsWith("https://") ? https : http;
 
-  return new Promise(function(resolve, reject) {
-    protocolModule.get(url, function(response) {
-      let data = '';
+  return new Promise(function (resolve, reject) {
+    protocolModule
+      .get(url, function (response) {
+        let data = "";
 
-      response.on('data', function(chunk) {
-        data += chunk;
-      });
+        response.on("data", function (chunk) {
+          data += chunk;
+        });
 
-      response.on('end', function() {
-        resolve(data);
-      });
+        response.on("end", function () {
+          resolve(data);
+        });
 
-      response.on('error', function(error) {
+        response.on("error", function (error) {
+          reject(error);
+        });
+      })
+      .on("error", function (error) {
         reject(error);
       });
-    }).on('error', function(error) {
-      reject(error);
-    });
   });
 }
 
-// Function to read data from file and fetch URLs
 function fetchDataFromFile(filePath) {
-  return new Promise(function(resolve, reject) {
-    fs.readFile(filePath, 'utf8', function(err, fileData) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, "utf8", function (err, fileData) {
       if (err) {
         reject(err);
       } else {
-        const urls = fileData.split('\n').map(function(url) {
+        const urls = fileData.split("\n").map(function (url) {
           return url.trim();
         });
         resolve(urls);
@@ -43,10 +43,9 @@ function fetchDataFromFile(filePath) {
   });
 }
 
-// Function to save data to file
 function saveDataToFile(fileName, data) {
-  return new Promise(function(resolve, reject) {
-    fs.writeFile(fileName, data, 'utf8', function(err) {
+  return new Promise(function (resolve, reject) {
+    fs.writeFile(fileName, data, "utf8", function (err) {
       if (err) {
         reject(err);
       } else {
@@ -56,28 +55,28 @@ function saveDataToFile(fileName, data) {
   });
 }
 
-// Function to handle the fetching and saving of data for each URL
 async function processUrls(urls) {
   for (let i = 0; i < urls.length; i++) {
     const url = urls[i];
     try {
       const response = await makeHttpRequest(url);
-      const fileName = `url_${i}.json`;
+      var x = i.toString();
+      const fileName = "url_" + x + ".json";
       await saveDataToFile(fileName, response);
-      console.log(`Data from ${url} saved to ${fileName}`);
+
+      console.log("data from ", url, "saved to", fileName);
     } catch (error) {
-      console.error(`Error fetching data from ${url}:`, error);
+      console.error("error fetcing from file", error);
     }
   }
 }
 
-// Usage example
-const filePath = 'urls.txt';
+const filePath = "urls.txt";
 
 fetchDataFromFile(filePath)
-  .then(function(urls) {
+  .then(function (urls) {
     processUrls(urls);
   })
-  .catch(function(error) {
-    console.error('Error reading file:', error);
+  .catch(function (error) {
+    console.error("Error reading file:", error);
   });
