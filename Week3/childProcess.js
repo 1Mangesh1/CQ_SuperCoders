6f13cbd17c55e8954bade94124dcaa05da5f1457
Mangesh Bide <mangeshbide123@gmail.com>
2023-07-11 10:49:06 +0530

    something

---
+++ Week3/childProcess.js
@@ -0,0 +1,38 @@
+// const childProcess = require('child_process');
+// console.log(process.platform);
+// const command = childProcess.spawn("node", ["childProcess2.js"]);
+// // const command = childProcess.spawn("dir");
+
+// command.stdout.on('data', function(data) {
+//     data = data.toString();
+//     console.log(data);
+// });
+
+// command.on('close', function(code) {
+//     console.log(`child process exited with code ${code}`);
+// }
+// );
+
+// Path: Week3\childProcess2.js
+const childProcess = require("child_process");
+
+//const command = childProcess.spawn("ls", ["-a"]);
+
+let command = null;
+
+if (process.platform === "win32") {
+  command = childProcess.spawn("cmd", ["/c", "dir"]);
+} else {
+  command = childProcess.spawn("ls", ["-a"]);
+}
+
+command.stdout.on("data", function (data) {
+  console.log(data.toString());
+});
+
+command.on("close", function (code) {
+  console.log(`child process exited with code ${code}`);
+});
+command.on("error", function (err) {
+  console.log(`child process exited with error ${err}`);
+});
---
+++ Week3/envVars.js
@@ -0,0 +1,2 @@
+console.log(process.argv[3]);
+//https://github.com/nikhil2882/suoercoders-global/tree/lecture_7
