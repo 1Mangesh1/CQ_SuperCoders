611a48ffa21eddd1688d045e7f3b9f349f6eed77
Mangesh Bide <mangeshbide123@gmail.com>
2023-07-31 20:58:56 +0530

    todoejs done

--- Week5/Assignments/todoejs/todo/public/script/todoScript.js
+++ Week5/Assignments/todoejs/todo/public/script/todoScript.js
@@ -166,20 +166,3 @@ function updateStatusText(checked, todoDiv) {
 }
 
 
-fetch(("/username")
-,{
-    method: "get",
-    headers: {
-      "Content-Type": "text/plain",
-    },
-  }).then(function (response) {
-    if (response.status === 200) {
-      return response.text();
-    } else {
-      alert("Something went wrong");
-    }
-  }
-  ).then(function (username) {
-    document.getElementById("username").innerText = username;
-  }
-  );
